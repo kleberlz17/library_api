544b037531a9b4f37a1a1f3866217003be996ad4
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-02-12 01:57:00 -0300

    atualizando

--- src/main/java/kleberlz/libraryapi/controller/AutorController.java
+++ src/main/java/kleberlz/libraryapi/controller/AutorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -95,5 +96,25 @@ public class AutorController {
 		
 		return ResponseEntity.ok(lista);
 	  
+	}
+	@PutMapping("{id}")
+	public ResponseEntity<Void> atualizar(
+			@PathVariable("id") String id, @RequestBody AutorDTO dto){
+		
+		var idAutor = UUID.fromString(id);
+		Optional<Autor>autorOptional = service.obterPorId(idAutor);
+		
+		if(autorOptional.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		var autor = autorOptional.get();
+		autor.setNome(dto.nome());
+		autor.setNacionalidade(dto.nacionalidade());
+		autor.setDataNascimento(dto.dataNascimento());
+		
+		service.atualizar(autor);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
---
+++ src/main/java/kleberlz/libraryapi/controller/dto/ErroCampo.java
@@ -0,0 +1,5 @@
+package kleberlz.libraryapi.controller.dto;
+
+public record ErroCampo(String campo, String erro) {
+
+}
