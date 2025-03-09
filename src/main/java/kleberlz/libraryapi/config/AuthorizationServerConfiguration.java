9c2e5d4069062f3eb8248036e44b412271745800
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2025-03-09 01:09:40 -0300

    atualização

--- src/main/java/kleberlz/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/kleberlz/libraryapi/config/AuthorizationServerConfiguration.java
@@ -5,6 +5,8 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
@@ -13,15 +15,19 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -29,6 +35,8 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
+import kleberlz.libraryapi.security.CustomAuthentication;
 
 @Configuration
 @EnableWebSecurity
@@ -118,5 +126,26 @@ public class AuthorizationServerConfiguration {
 				// logout
 				.oidcLogoutEndpoint("/oauth2/logout")
 				.build();
+	}
+	
+	@Bean // ABAIXO - CÓDIGO PARA CUSTOMIZAR AS INFORMAÇÕES DO TOKEN
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+		return context -> {
+			var principal = context.getPrincipal();
+			
+			if(principal instanceof CustomAuthentication authentication) { // VE SE É UMA  CUSTOM AUTHENTICATION
+				OAuth2TokenType tipoToken = context.getTokenType();
+				
+				if(OAuth2TokenType.ACCESS_TOKEN.equals(tipoToken)) { // VÊ SE É UM ACCESS TOKEN
+					Collection<GrantedAuthority> authorities = authentication.getAuthorities();  // ABAIXO PEGA AS AUTHORITIES 
+					List<String> authoritiesList = 
+							authorities.stream().map(GrantedAuthority::getAuthority).toList(); 
+					
+					context.getClaims() // BOTA DENTRO DO CONTEXTO AS AUTHORITIES.
+					.claim("authorities", authoritiesList)
+					.claim("email", authentication.getUsuario().getEmail());
+				}
+			}
+		};
 	}
 }
